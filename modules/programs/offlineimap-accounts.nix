179d50173036736eaec965ff9c3a26bce3cc8f44
Robert Helgesson <robert@rycee.net>
2018-08-22 23:44:16 +0200

    wip

--- modules/programs/offlineimap-accounts.nix
+++ modules/programs/offlineimap-accounts.nix
@@ -11,14 +11,13 @@ with lib;
       default = "";
       description = ''
         Extra configuration lines added on a per-account basis.
-        '';
+      '';
     };
 
     postSyncHookCommand = mkOption {
       type = types.lines;
       default = "";
-      description = "command to run after fetching new mails";
+      description = "Command to run after fetching new mails.";
     };
   };
 }
-
